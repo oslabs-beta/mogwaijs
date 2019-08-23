294346beb7de90e15d004bf9de9d388c7fe78ba9
Taylor Burrington <taylor.burrington@gmail.com>
2019-08-23 15:28:24 -0700

    create edge starter code

--- functions/crud.js
+++ functions/crud.js
@@ -36,3 +36,42 @@ const sam = User.create(
   });
 
 console.log(sam);
+
+/**
+ * 
+ * @param {String} label - "the edge type: required;" 
+ * @param {Object} schema - "an object containing property names as keys; values are the constructor for the type of data contained at this key"
+ */
+
+function EdgeModel(label, schema = {}) {
+  this.label = label;
+
+
+  Object.entries(schema).forEach(keyValuePair => {
+    this[keyValuePair[0]] = keyValuePair[1];
+  })
+}
+
+/**
+ * @param {Object} firstNode -"the name of the first node in the relationship which points to the second node"
+ * @param {Object} secondNode-"the name of the second node in the relationship; this node is pointed to"
+ * @param {Object} props-"an object containing property names as keys; values are the constructor for the type of data contained at this key"
+ */
+EdgeModel.prototype.createEdge = function createEdge(fromNode, toNode, props) {
+  let resultString;
+
+  let initialString = 'g.V()';
+  let middleString = `.addE('${this.label})`;
+  let endingString = 'g.V()';
+
+  //firstNode -- [relationship; props] --> secondNode
+  Object.entries(fromNode).forEach(keyValuePair => {
+   initialString += `.hasProperty('${keyValuePair[0]}', '${keyValuePair[1]}')`});
+
+  Object.entries(toNode).forEach(keyValuePair => {
+    endingString += `.hasProperty('${keyValuePair[0]}', '${keyValuePair[1]}')`});
+     
+  resultString = initialString + middleString+ endingString;
+  return resultString;
+   
+  };
