373b3bd879df581ab1b7394801913eadf769e3ad
Tanner Houghton <Tanner@Tanners-MacBook-Pro-2.local>
2019-08-27 15:29:19 -0700

    The tests now verify that upon the invocation of Client.submit, a promise is returned

--- test/test.js
+++ test/test.js
@@ -1,5 +1,8 @@
-var assert = require('assert');
+const assert = require('assert');
 const vertex = require('../functions/crud')
+const EdgeModel = require('../functions/edgeModel')
+
+// VertexModel Tests
 
 describe('VertexModel', function() {
   it('should generate a gremlin string without modifying the model', function() {
@@ -8,24 +11,53 @@ describe('VertexModel', function() {
     modelBeforeRunningFunc.create({name: 'Tanner'})
     assert.equal(modelBeforeRunningFunc, copy)
   })
+  it('should output an object', () => {
+    let testModel = vertex.VertexModel('test')
+    assert.equal(typeof testModel, "object")
+  })
 })
 
-describe('VertexModel.create', () => {
+describe('createVertex', () => {
     it('should return a string', function() {
       let testGremlinSting = vertex.VertexModel.create('test')
       assert.equal(typeof testGremlinSting, "string")
     })
 })
 
-describe('EdgeModel', function() {
-  it('should create an Edge Model',function () {
-    let testModel = vertex.EdgeModel('test')
-    assert.equal(typeof(testModel), "object")
+describe('findVertexByProps', () => {
+  it('should return a promise', () => {
+    let exampleFindVertedByProps = vertex.findVertexByProps();
+    assert.equal(exampleFindVertedByProps instanceof Promise, true)
   })
 })
 
-describe('Validate Props', function() {
-  it('should create Edge Model constructor',function () {
-    
+// EdgeModel Tests
+
+describe('EdgeModel', function() {
+  describe('create Edge Model', () => {
+    it('should output an object', () => {
+      let testModel = EdgeModel.EdgeModel('test')
+      assert.equal(typeof testModel, "object")
+    })
+  })
+  describe('createEdge', () => {
+    it('should output a string',function () {
+      let testCreateEdge = EdgeModel.createEdge()
+      assert.equal(typeof testCreateEdge, 'string')
+    })
+    it('should return a promise', () => {
+      let exampleCreateEdge = EdgeModel.createEdge();
+      assert.equal(exampleCreateEdge instanceof Promise, true)
+    })
+  })
+  describe('addPropsToEdge', () => {
+    it('should output a string',function () {
+      let testAddPropsToEdge = EdgeModel.addPropsToEdge()
+      assert.equal(typeof testAddPropsToEdge, 'string')
+    })
+    it('should return a promise', () => {
+      let exampleaddPropsToEdge = EdgeModel.addPropsToEdge();
+      assert.equal(exampleaddPropsToEdge instanceof Promise, true)
+    })
   })
 })
